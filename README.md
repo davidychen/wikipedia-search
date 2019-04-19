705fcbb70e96a8d7a7f1e451877a095c53259b70
David Chen <chen.yifei@husky.neu.edu>
2019-04-19 12:18:28 -0700

    readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # wikipedia-search
-CS 5610 Final
+
+A navigation tool for Wikipedia, allowing history + recommendation links. 
+
+# Requires
+
+Node
+meteor
+react
+
+# Usage
+
+Clone the repo, then open the terminal on the folder created and run
+
+```
+git clone https://github.com/davidychen/wikipedia-search.git
+cd wikipedia-search
+meteor npm install
+meteor reset
+meteor
+```
+
+Which will run the front-end development server on the port 3000, then visit (http://localhost:3000) and you should see the app running. The database is running on port 3001.
+
+# Author
+
+[Yifei Chen](http://davidychen.com/HomepageDavidChen/)
+
+# Class Link
+
+[CS-5610 Web Development Spring 2019](http://johnguerra.co/classes/webDevelopment_spring_2019/)
+
+# License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- imports/api/wiki.js
+++ imports/api/wiki.js
@@ -3,46 +3,9 @@ import { Meteor } from "meteor/meteor";
 import { HTTP } from "meteor/http";
 import { check } from "meteor/check";
 
+
+
 Meteor.methods({
-  wiki(search) {
-    check(search, String);
-    let wikipedia = require("node-wikipedia");
-    wikipedia.page.data("Clifford_Brown", { content: true }, function(
-      response
-    ) {
-      console.log(response);
-      // structured information on the page for Clifford Brown (wikilinks, references, categories, etc.)
-    });
-    // Meteor.call("...", (err, res) => {....})
-    if (Meteor.isServer) {
-      this.unblock();
-      const url = "https://en.wikipedia.org/w/api.php";
-      let params = {
-        action: "query",
-        format: "json",
-        list: "search",
-        prop: "info",
-        inprop: "url",
-        alunique: 1,
-        utf8: "",
-        origin: "*",
-        srsearch: search,
-        srlimit: 100
-      };
-      try {
-        // let params = { address: streetAddress, key: GOOGLE_API_KEY };
-        let response = HTTP.call("GET", url, { params: params });
-        // console.log("RESPONSE", JSON.parse(response.content));
-        return JSON.parse(response.content);
-        // let fullJSON = JSON.parse(response.content);
-        // console.log("ADDRESSS LOCATION PARSE JSON", fullJSON);
-        //console.log(fullJSON);
-        // return fullJSON.results[0].geometry.location;
-      } catch (e) {
-        console.log("address error" + e);
-      }
-    }
-  },
   wikiSearch(term) {
     check(term, String);
 
